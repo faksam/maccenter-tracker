e853758422fb6f47acd6fd3b233e0b907e3ab442
faksam <fakunlesamuel@gmail.com>
2018-01-17 10:09:39 +0100

    create angular project

---
+++ client/app/app.module.ts
@@ -0,0 +1,54 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+
+
+import { RoutingModule } from './routing.module';
+import { SharedModule } from './shared/shared.module';
+import { CatService } from './services/cat.service';
+import { UserService } from './services/user.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuardLogin } from './services/auth-guard-login.service';
+import { AuthGuardAdmin } from './services/auth-guard-admin.service';
+import { AppComponent } from './app.component';
+import { CatsComponent } from './cats/cats.component';
+import { AboutComponent } from './about/about.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AccountComponent } from './account/account.component';
+import { AdminComponent } from './admin/admin.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CatsComponent,
+    AboutComponent,
+    RegisterComponent,
+    LoginComponent,
+    LogoutComponent,
+    AccountComponent,
+    AdminComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    RoutingModule,
+    SharedModule,
+    BrowserModule.withServerTransition({ appId: 'tour-of-heroes' }),
+  ],
+  providers: [
+    AuthService,
+    AuthGuardLogin,
+    AuthGuardAdmin,
+    CatService,
+    UserService
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
